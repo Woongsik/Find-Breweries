ab75fe9060d88ca0e3451b1847d58c99fc324502
Woongsik <airwoong83@gmail.com>
2024-02-09 00:53:46 +0200

    Add SearchInput

--- src/components/searchInput/SearchInput.tsx
+++ src/components/searchInput/SearchInput.tsx
@@ -1,38 +1,55 @@
 import React, { useCallback, useState } from 'react';
+import './SearchInput.css';
 
-export default function SearchInput() {
+type Props = {
+  searchChanged: Function
+}
+export default function SearchInput(props: Props) {
   const [searchPhrase, setSearchPhrase] = useState<string>('');
-
-  const debounce = (() => {
-    console.log('debounce', searchPhrase);
-    let taskInQue: any;
-
-    return (callback: Function, delayTime: number) => {
-      console.log('task', taskInQue, typeof taskInQue);
-      if (taskInQue) {
-        clearTimeout(taskInQue);
-      }
-
-      taskInQue = setTimeout(() => {
-        callback();
-      }, delayTime);
+  
+  // debounce manually
+  const debounce = (callback: Function, delay: number) => {
+    let timeout: any;
+    return (value: string) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        callback(value);
+      }, delay);
     }
-  })();
+  }
 
-  const submitInputValue = useCallback(() => {
-    console.log('submit!');
-  }, [searchPhrase]);
+  const submitInputValue = (value: string) => {
+    console.log('submit the input:', value);
+    props.searchChanged(value);
+  }
+
+  const debounced = debounce(submitInputValue, 1000);
+  const debouncedCallback = useCallback((value: string) => debounced(value), []);
+
+  const setNewSearch = (value: string) => {
+    setSearchPhrase(value);
+    debouncedCallback(value);
+  }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
-    setSearchPhrase(value);
-    debounce(submitInputValue, 3000);
-  } 
+    setNewSearch(value);
+  }
+
+  const clear = () => {
+    console.log('Clear');
+    setNewSearch('');
+  }
 
   return (
-    <div>
-      <input onChange={handleChange}/>
-      Result: {searchPhrase}
+    <div className="search">
+      <input
+        value={searchPhrase}
+        onChange={handleChange}
+        className="search__input" />
+        <button onClick={clear}>
+          Clear  
+        </button> 
     </div>
   )
 };
