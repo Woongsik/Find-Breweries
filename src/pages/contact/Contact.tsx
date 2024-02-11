8fbd7aed62a99209c17254d0f089e23be30ad1cf
Woongsik <airwoong83@gmail.com>
2024-02-11 17:16:09 +0200

    Add form and install react-hook-form

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Form } from '../../components/form/Form';
+import PageTitle from '../../components/pageTitle/PageTitle';
+import './Contact.css';
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    // use React hook form to handle the contact form, add validation and display error
+    <div className="contact">
+      <div>
+      <PageTitle title='Contac us!' />
+      <Form />
+      </div>
+    </div>
   )
 }
